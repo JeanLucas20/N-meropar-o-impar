cecfe1ba005a8a3f84e865a861e928425843e102
Jean Lucas Nova Garcìa <102611326+JeanLucas20@users.noreply.github.com>
2022-04-25 09:36:11 -0500

    Add files via upload

---
+++ Número_par_o_impar/Par_impar.py
@@ -0,0 +1,12 @@
+# Ejercicio PPara saber si un número es par o impar
+
+### Input
+a= int(input("Ingresa un número: "))
+
+
+### Processing
+if a % 2 == 0:
+
+    b= print("El número " + str(a)+ " es par") ### Output
+else:
+    b= print("El número " + str(a)+ " es impar") ### Output
---
+++ Número_par_o_impar/README.md
@@ -0,0 +1,7 @@
+# Programa para saber si un número es par o impar
+
+## Ejercicio para saber si un número es par o impar
+
+### Diagrama de flujo
+
+![diagrama de flujo](numero.png "Diagrama de flujo")
